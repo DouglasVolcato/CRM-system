e013e861234309661aec9d1536ae79ea7bcbeb04
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-05 13:19:49 -0300

    Added return to methods

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -10,26 +10,26 @@ export class UserController {
 
   createUserController(req: { body: UserInterface }, res: any) {
     const body = req.body;
-    const newUser = this.services.CreateUserUsecase.execute(body);
-    res.send(newUser);
+    const newUser = this.services.createUserUsecase.execute(body);
+    return res.send(newUser);
   }
 
   deleteUserController(req: { params: { id: string } }, res: any) {
     const id = Number(req.params.id);
-    const deletedUser = this.services.DeleteUserUsecase.execute(id);
-    res.send(deletedUser);
+    const deletedUser = this.services.deleteUserUsecase.execute(id);
+    return res.send(deletedUser);
   }
 
   getUserByIdController(req: { params: { id: string } }, res: any) {
     const id = Number(req.params.id);
-    const foundUser = this.services.GetUserByIdUseCase.execute(id);
-    res.send(foundUser);
+    const foundUser = this.services.getUserByIdUseCase.execute(id);
+    return res.send(foundUser);
   }
 
   getUserByNameController(req: { params: { name: string } }, res: any) {
     const name = req.params.name;
-    const foundUser = this.services.GetUserByNameUseCase.execute(name);
-    res.send(foundUser);
+    const foundUser = this.services.getUserByNameUseCase.execute(name);
+    return res.send(foundUser);
   }
 
   updateUserController(
@@ -38,7 +38,7 @@ export class UserController {
   ) {
     const id = Number(req.params.id);
     const body = req.body;
-    const updatedUser = this.services.UpdateUserUseCase.execute(id, body);
-    res.send(updatedUser);
+    const updatedUser = this.services.updateUserUseCase.execute(id, body);
+    return res.send(updatedUser);
   }
 }
