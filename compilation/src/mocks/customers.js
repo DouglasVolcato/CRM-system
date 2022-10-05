ff0bbff33cdfcb74ab5964056bdee401650bf86f
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-05 19:34:16 -0300

    Added new mocked customer

--- compilation/src/mocks/customers.js
+++ compilation/src/mocks/customers.js
@@ -1,4 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.customers = void 0;
-exports.customers = [];
+exports.customers = [
+    {
+        id: 1,
+        name: "Douglas",
+        age: 22,
+        phone: 51999772868,
+        city: "Porto Alegre",
+        notes: "Empty",
+    },
+];
--- src/mocks/customers.ts
+++ src/mocks/customers.ts
@@ -1,3 +1,12 @@
 import { CustomerInterface } from "../interfaces/entities.interfaces/customer.interface";
 
-export const customers: CustomerInterface[] = [];
+export const customers: CustomerInterface[] = [
+  {
+    id: 1,
+    name: "Douglas",
+    age: 22,
+    phone: 51999772868,
+    city: "Porto Alegre",
+    notes: "Empty",
+  },
+];
