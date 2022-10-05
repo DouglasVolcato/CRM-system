9ecedd3f11e14ee300aa04cf7671d07c51482479
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-05 13:20:55 -0300

    Removed static property in methods

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -2,20 +2,20 @@ import { UserInterface } from "../interfaces/entities.interfaces/user.interface"
 import { users } from "../mocks/user";
 
 export class UserRepository {
-  static createUser(userBody: UserInterface) {
+  createUser(userBody: UserInterface) {
     users.push(userBody);
     return { message: "User created." };
   }
 
-  static getUserByName(userName: string) {
+  getUserByName(userName: string) {
     return users.filter((user) => user.name === userName);
   }
 
-  static getUserById(userId: number) {
+  getUserById(userId: number) {
     return users.filter((user) => user.id === userId);
   }
 
-  static deleteUser(userId: number) {
+  deleteUser(userId: number) {
     users.map((user, index) => {
       if (user.id === userId) {
         users.splice(index, 1);
@@ -24,7 +24,7 @@ export class UserRepository {
     return { message: "User deleted." };
   }
 
-  static updateUser(userId: number, userBody: UserInterface) {
+  updateUser(userId: number, userBody: UserInterface) {
     users.map((user, index) => {
       if (user.id === userId) {
         users.splice(index, 1, userBody);
