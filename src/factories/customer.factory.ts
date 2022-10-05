914a4b799ffb5c325b2de6cda6158e79d27d9807
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-05 13:21:11 -0300

    Added customer factory

---
+++ src/factories/customer.factory.ts
@@ -0,0 +1,36 @@
+import { CustomerController } from "../controllers/customer.controller";
+import { CustomerRepository } from "../repositories/customer.repository";
+import { CustomerRoutes } from "../routes/customer.routes";
+import * as services from "../services/customer.services.index";
+
+export function makeCustomerFactory(router: any) {
+  const customerRepository = new CustomerRepository();
+
+  const createCustomerUseCase = new services.CreateCustomerUseCase(
+    customerRepository
+  );
+  const deleteCustomerUseCase = new services.DeleteCustomerUseCase(
+    customerRepository
+  );
+  const getCustomerByIdUseCase = new services.GetCustomerByIdUseCase(
+    customerRepository
+  );
+  const getCustomerByNameUseCase = new services.GetCustomerByNameUseCase(
+    customerRepository
+  );
+  const updateCustomerUseCase = new services.UpdateCustomerUseCase(
+    customerRepository
+  );
+
+  const customerController = new CustomerController({
+    createCustomerUseCase,
+    deleteCustomerUseCase,
+    getCustomerByIdUseCase,
+    getCustomerByNameUseCase,
+    updateCustomerUseCase,
+  });
+
+  const customerRoutes = new CustomerRoutes(customerController, router);
+
+  return customerRoutes;
+}
