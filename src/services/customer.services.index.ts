83bf5fb65de047ba330b2a4c11505f679749bfc9
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-05 02:50:49 -0300

    Added customer services indes

---
+++ src/services/customer.services.index.ts
@@ -0,0 +1,5 @@
+export { CreateCustomerUseCase } from "./customer/createCustomer";
+export { DeleteCustomerUseCase } from "./customer/deleteCustomer";
+export { GetCustomerByIdUseCase } from "./customer/getCustomerById";
+export { GetCustomerByNameUseCase } from "./customer/getCustomerByName";
+export { UpdateCustomerUseCase } from "./customer/updateCustomer";
