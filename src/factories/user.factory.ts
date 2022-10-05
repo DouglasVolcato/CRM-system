088b44837cc7278d6c8792f3ad575a22126dec93
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-05 13:21:22 -0300

    Added user factory

---
+++ src/factories/user.factory.ts
@@ -0,0 +1,28 @@
+import { UserController } from "../controllers/user.controller";
+import { UserRepository } from "../repositories/user.repository";
+import { UserRoutes } from "../routes/user.routes";
+import * as services from "../services/user.services.index";
+
+export function makeUserFactory(router: any) {
+  const userRepository = new UserRepository();
+
+  const createUserUsecase = new services.CreateUserUsecase(userRepository);
+  const deleteUserUsecase = new services.DeleteUserUsecase(userRepository);
+  const getUserByIdUseCase = new services.GetUserByIdUseCase(userRepository);
+  const getUserByNameUseCase = new services.GetUserByNameUseCase(
+    userRepository
+  );
+  const updateUserUseCase = new services.UpdateUserUseCase(userRepository);
+
+  const userController = new UserController({
+    createUserUsecase,
+    deleteUserUsecase,
+    getUserByIdUseCase,
+    getUserByNameUseCase,
+    updateUserUseCase,
+  });
+
+  const userRoutes = new UserRoutes(userController, router);
+
+  return userRoutes;
+}
