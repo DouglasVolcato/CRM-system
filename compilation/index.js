91e45df15069ed9f01e65eed70c691ccfad5226b
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-05 19:37:20 -0300

    Added js compilation

--- compilation/index.js
+++ compilation/index.js
@@ -1,1 +1,43 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const customer_factory_1 = require("./src/factories/customer.factory");
+const user_factory_1 = require("./src/factories/user.factory");
+const http = __importStar(require("http"));
+const server = http.createServer((req, res) => {
+    const router = req.url;
+    if (router === null || router === void 0 ? void 0 : router.includes("/users/")) {
+        (0, user_factory_1.makeUserFactory)(router, req, res);
+    }
+    else if (router === null || router === void 0 ? void 0 : router.includes("/customers/")) {
+        (0, customer_factory_1.makeCustomerFactory)(router, req, res);
+    }
+    else {
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Route not found" }));
+    }
+});
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log("http://localhost:" + port));
