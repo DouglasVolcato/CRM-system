5084b5cd875eaf173af80039a652100cb3b26ce0
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-06 03:20:24 -0300

    Updated returns to UserServicesInterface

--- src/interfaces/services.interfaces/user.services.interface.ts
+++ src/interfaces/services.interfaces/user.services.interface.ts
@@ -2,22 +2,25 @@ import { UserInterface } from "../entities.interfaces/user.interface";
 
 export interface UserServicesInterface {
   createUserUsecase: {
-    execute(userBody: UserInterface): { message: string };
+    execute(userBody: UserInterface): Promise<unknown> | { message: string };
   };
 
   deleteUserUsecase: {
-    execute(userId: number): { message: string };
+    execute(userId: number): Promise<unknown> | { message: string };
   };
 
   getUserByIdUseCase: {
-    execute(userId: number): UserInterface[];
+    execute(userId: number): Promise<unknown> | UserInterface[];
   };
 
   getUserByNameUseCase: {
-    execute(userName: string): UserInterface[];
+    execute(userName: string): Promise<unknown> | UserInterface[];
   };
 
   updateUserUseCase: {
-    execute(userId: number, userBody: UserInterface): { message: string };
+    execute(
+      userId: number,
+      userBody: UserInterface
+    ): Promise<unknown> | { message: string };
   };
 }
