43a20026c1dcaa950f30d158a8f178be4d323d7b
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-06 03:20:46 -0300

    Updated returns to CustomerServicesInterface

--- src/interfaces/services.interfaces/customer.services.interface.ts
+++ src/interfaces/services.interfaces/customer.services.interface.ts
@@ -2,25 +2,27 @@ import { CustomerInterface } from "../entities.interfaces/customer.interface";
 
 export interface CustomerServicesInterface {
   createCustomerUseCase: {
-    execute(customerBody: CustomerInterface): { message: string };
+    execute(
+      customerBody: CustomerInterface
+    ): Promise<unknown> | { message: string };
   };
 
   deleteCustomerUseCase: {
-    execute(customerId: number): { message: string };
+    execute(customerId: number): Promise<unknown> | { message: string };
   };
 
   getCustomerByIdUseCase: {
-    execute(customerId: number): CustomerInterface[];
+    execute(customerId: number): Promise<unknown> | CustomerInterface[];
   };
 
   getCustomerByNameUseCase: {
-    execute(customerName: string): CustomerInterface[];
+    execute(customerName: string): Promise<unknown> | CustomerInterface[];
   };
 
   updateCustomerUseCase: {
     execute(
       customerId: number,
       customerBody: CustomerInterface
-    ): { message: string };
+    ): Promise<unknown> | { message: string };
   };
 }
