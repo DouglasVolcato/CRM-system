cdb9a96c30ef778b09208f92c7cdb867689eb875
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-06 03:21:10 -0300

    Updated returns to UserControllerInterface

--- src/interfaces/controllers.interfaces/user.controller.interface.ts
+++ src/interfaces/controllers.interfaces/user.controller.interface.ts
@@ -4,25 +4,25 @@ export interface UserControllerInterface {
   createUserController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): http.IncomingMessage;
+  ): Promise<unknown> | http.IncomingMessage;
 
   deleteUserController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): http.ServerResponse;
+  ): Promise<unknown> | http.ServerResponse;
 
   getUserByIdController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): http.ServerResponse;
+  ): Promise<unknown> | http.ServerResponse;
 
   getUserByNameController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): http.ServerResponse;
+  ): Promise<unknown> | http.ServerResponse;
 
   updateUserController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): http.IncomingMessage;
+  ): Promise<unknown> | http.IncomingMessage;
 }
