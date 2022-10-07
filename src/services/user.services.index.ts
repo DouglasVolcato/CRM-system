396d6c22a6541249feae27cfd481d8dea1ff04de
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-07 00:25:59 -0300

    Added getAllUsers service

--- src/services/user.services.index.ts
+++ src/services/user.services.index.ts
@@ -1,5 +1,6 @@
 export { CreateUserUsecase } from "./user/createUser";
 export { DeleteUserUsecase } from "./user/deleteUser";
+export { GetAllUserUseCase } from "./user/getAllUser";
 export { GetUserByIdUseCase } from "./user/getUserById";
 export { GetUserByNameUseCase } from "./user/getUserByName";
 export { UpdateUserUseCase } from "./user/updateUser";
