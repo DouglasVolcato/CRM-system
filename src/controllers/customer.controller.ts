f807f62d2a7dcfc9d9e0a26ce6edb63f06317d22
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-07 00:31:43 -0300

    Added getAllCustomersController

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -43,7 +43,7 @@ export class CustomerController {
     req: http.IncomingMessage,
     res: http.ServerResponse
   ) {
-    const id = Number(req.url?.replace("/users/delete-user/", ""));
+    const id = Number(req.url?.replace("/customers/delete-customers/", ""));
     const deletedCustomer = await this.services.deleteCustomerUseCase.execute(
       id
     );
@@ -51,11 +51,20 @@ export class CustomerController {
     return res.end(JSON.stringify(deletedCustomer));
   }
 
+  async getAllCustomersController(
+    req: http.IncomingMessage,
+    res: http.ServerResponse
+  ) {
+    const foundCustomers = await this.services.getAllCustomerUseCase.execute();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify(foundCustomers));
+  }
+
   async getCustomerByIdController(
     req: http.IncomingMessage,
     res: http.ServerResponse
   ) {
-    const id = Number(req.url?.replace("/users/find-user-by-id/", ""));
+    const id = Number(req.url?.replace("/customers/find-customers-by-id/", ""));
     const foundCustomer = await this.services.getCustomerByIdUseCase.execute(
       id
     );
@@ -68,7 +77,7 @@ export class CustomerController {
     res: http.ServerResponse
   ) {
     if (req.url) {
-      const name = req.url.replace("/users/find-user-by-name/", "");
+      const name = req.url.replace("/customers/find-customers-by-name/", "");
       const foundCustomer =
         await this.services.getCustomerByNameUseCase.execute(name);
       res.writeHead(200, { "Content-Type": "application/json" });
