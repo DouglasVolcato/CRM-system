ab567538158dd3106c322a41958d7678636beafb
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-07 00:30:26 -0300

    Added getAllUsers

--- src/interfaces/repositories.interfaces/user.repository.interface.ts
+++ src/interfaces/repositories.interfaces/user.repository.interface.ts
@@ -1,13 +1,18 @@
 import { UserInterface } from "../entities.interfaces/user.interface";
 
 export interface UserRepositoryInterface {
-  createUser(userBody: UserInterface): { message: string };
+  createUser(userBody: UserInterface): Promise<unknown> | UserInterface;
 
-  getUserByName(userName: string): UserInterface[];
+  getAllUsers(): Promise<unknown> | UserInterface[];
 
-  getUserById(userId: number): UserInterface[];
+  getUserByName(userName: string): Promise<unknown> | UserInterface[];
 
-  deleteUser(userId: number): { message: string };
+  getUserById(userId: number): Promise<unknown> | UserInterface;
 
-  updateUser(userId: number, userBody: UserInterface): { message: string };
+  deleteUser(userId: number): Promise<unknown> | UserInterface;
+
+  updateUser(
+    userId: number,
+    userBody: UserInterface
+  ): Promise<unknown> | UserInterface;
 }
