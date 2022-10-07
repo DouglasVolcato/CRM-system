ec558a25a18801ea225648b49a70b44e6ceca534
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-07 00:26:20 -0300

    Added getAllCustomers service

--- src/services/customer.services.index.ts
+++ src/services/customer.services.index.ts
@@ -1,5 +1,6 @@
 export { CreateCustomerUseCase } from "./customer/createCustomer";
 export { DeleteCustomerUseCase } from "./customer/deleteCustomer";
+export { GetAllCustomerUseCase } from "./customer/getAllCustomer";
 export { GetCustomerByIdUseCase } from "./customer/getCustomerById";
 export { GetCustomerByNameUseCase } from "./customer/getCustomerByName";
 export { UpdateCustomerUseCase } from "./customer/updateCustomer";
