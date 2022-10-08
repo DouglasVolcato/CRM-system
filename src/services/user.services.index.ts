e1f83b26a02c6c2e10c1d93908fdfccfb9daf28a
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-08 19:31:08 -0300

    Added GetUserByEmailUseCase

--- src/services/user.services.index.ts
+++ src/services/user.services.index.ts
@@ -2,5 +2,6 @@ export { CreateUserUsecase } from "./user/createUser";
 export { DeleteUserUsecase } from "./user/deleteUser";
 export { GetAllUserUseCase } from "./user/getAllUser";
 export { GetUserByIdUseCase } from "./user/getUserById";
+export { GetUserByEmailUseCase } from "./user/getUserByEmail";
 export { GetUserByNameUseCase } from "./user/getUserByName";
 export { UpdateUserUseCase } from "./user/updateUser";
