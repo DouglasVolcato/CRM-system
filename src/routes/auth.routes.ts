7b7b33b836eb05ea62ec34357ecd1d17ca498958
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-08 22:11:52 -0300

    Added AuthRoutes

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,30 @@
+import * as http from "http";
+import { AuthControllerInterface } from "../interfaces/controllers.interfaces/auth.controller.interface";
+
+export class AuthRoutes {
+  controller: AuthControllerInterface;
+  router: string;
+  req: http.IncomingMessage;
+  res: http.ServerResponse;
+
+  constructor(
+    controller: AuthControllerInterface,
+    router: string,
+    req: http.IncomingMessage,
+    res: http.ServerResponse
+  ) {
+    this.controller = controller;
+    this.router = router;
+    this.req = req;
+    this.res = res;
+  }
+
+  route() {
+    if (this.router === "/auth/login" && this.req.method === "POST") {
+      this.controller.loginController(this.req, this.res);
+    } else {
+      this.res.writeHead(400, { "Content-Type": "application/json" });
+      this.res.end(JSON.stringify({ message: "Route not found" }));
+    }
+  }
+}
