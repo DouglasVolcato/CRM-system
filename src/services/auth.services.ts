68b231daf794898f667af26f1dde46ed3c11b3fe
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-08 22:10:22 -0300

    Added auth services index

---
+++ src/services/auth.services.ts
@@ -0,0 +1,3 @@
+export { ComparePasswordUseCase } from "./auth/comparePassword";
+export { ValidateTokenUseCase } from "./auth/validateToken";
+export { GenerateTokenUseCase } from "./auth/generateToken";
