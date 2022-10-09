d822eee97c23e8de5fe36bb7e5a6fac8457c55fd
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-08 22:11:41 -0300

    Added AuthController

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,38 @@
+import { AuthServicesInterface } from "../interfaces/services.interfaces/auth.services.interface";
+import * as http from "http";
+import { statusCodeGenerator } from "../helpers/statusCodeGenerator";
+
+export class AuthController {
+  services: AuthServicesInterface;
+
+  constructor(services: AuthServicesInterface) {
+    this.services = services;
+  }
+
+  async loginController(req: http.IncomingMessage, res: http.ServerResponse) {
+    let body = "";
+
+    req.on("data", (chunk: any) => {
+      body += chunk.toString();
+    });
+
+    return req.on("end", async () => {
+      try {
+        const { email, password } = await JSON.parse(body);
+
+        await this.services.comparePasswordUseCase.execute(password, email);
+
+        const generatedToken =
+          this.services.generateTokenUseCase.execute(email);
+
+        res.writeHead(201, { "Content-Type": "application/json" });
+        return res.end(JSON.stringify({ token: generatedToken }));
+      } catch (err) {
+        res.writeHead(statusCodeGenerator(err), {
+          "Content-Type": "application/json",
+        });
+        return res.end(JSON.stringify({ Message: "Error in login: " + err }));
+      }
+    });
+  }
+}
