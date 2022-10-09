e149e721d817d8fe08e32f4b370e606e700340bf
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-08 22:10:09 -0300

    Added ComparePasswordUseCase

---
+++ src/services/auth/comparePassword.ts
@@ -0,0 +1,29 @@
+import { cryptography } from "../../helpers/cryptography";
+import { UserRepositoryInterface } from "../../interfaces/repositories.interfaces/user.repository.interface";
+
+export class ComparePasswordUseCase {
+  repository: UserRepositoryInterface;
+
+  constructor(repository: UserRepositoryInterface) {
+    this.repository = repository;
+  }
+
+  async execute(userPassword: string, userEmail: string): Promise<boolean> {
+    const foundUser: any = await this.repository.getUserByEmail(userEmail);
+
+    if (!foundUser) {
+      throw new Error("User not found.");
+    }
+
+    const result = cryptography.comparePassword(
+      userPassword,
+      foundUser.password
+    );
+
+    if (result === false) {
+      throw new Error("Invalid password.");
+    }
+
+    return result;
+  }
+}
