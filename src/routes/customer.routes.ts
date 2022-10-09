9fa76c04fd0d960da36e2fd1e8ebfb59861169bb
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-08 23:24:46 -0300

    Added middleware to customer routes

--- src/routes/customer.routes.ts
+++ src/routes/customer.routes.ts
@@ -1,5 +1,6 @@
 import { CustomerControllerInterface } from "../interfaces/controllers.interfaces/customer.controller.interface";
 import * as http from "http";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 export class CustomerRoutes {
   controller: CustomerControllerInterface;
@@ -19,36 +20,42 @@ export class CustomerRoutes {
     this.res = res;
   }
 
-  route() {
+  async route() {
     if (
       this.router === "/customers/create-customer" &&
       this.req.method === "POST"
     ) {
+      await authMiddleware(this.req, this.res);
       this.controller.createCustomerController(this.req, this.res);
     } else if (
       this.router.includes("/customers/delete-customer/") &&
       this.req.method === "DELETE"
     ) {
+      await authMiddleware(this.req, this.res);
       this.controller.deleteCustomerController(this.req, this.res);
     } else if (
       this.router.includes("/customers/find-customer-by-id/") &&
       this.req.method === "GET"
     ) {
+      await authMiddleware(this.req, this.res);
       this.controller.getCustomerByIdController(this.req, this.res);
     } else if (
       this.router === "/customers/find-all-customers" &&
       this.req.method === "GET"
     ) {
+      await authMiddleware(this.req, this.res);
       this.controller.getAllCustomersController(this.req, this.res);
     } else if (
       this.router.includes("/customers/find-customers-by-name/") &&
       this.req.method === "GET"
     ) {
+      await authMiddleware(this.req, this.res);
       this.controller.getCustomerByNameController(this.req, this.res);
     } else if (
       this.router.includes("/customers/update-customer/") &&
       this.req.method === "PUT"
     ) {
+      await authMiddleware(this.req, this.res);
       this.controller.updateCustomerController(this.req, this.res);
     } else {
       this.res.writeHead(400, { "Content-Type": "application/json" });
