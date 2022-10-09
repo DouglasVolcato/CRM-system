5e760b29105bf6416ad15ae7f488a711af23037e
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-08 23:24:26 -0300

    Added middleware for token verification

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,28 @@
+import * as http from "http";
+import { UserRepository } from "../repositories/user.repository";
+import { ValidateTokenUseCase } from "../services/auth.services.index";
+
+export async function authMiddleware(
+  req: http.IncomingMessage,
+  res: http.ServerResponse
+): Promise<void | http.ServerResponse<http.IncomingMessage>> {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    res.writeHead(400, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify({ message: "Invalid token." }));
+  }
+
+  const tokenValidation = await new ValidateTokenUseCase(
+    new UserRepository()
+  ).execute(token);
+
+  if (!tokenValidation) {
+    res.writeHead(400, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify({ message: "Invalid token." }));
+  }
+
+  req.headers = { authorization: "authorized" };
+
+  return;
+}
