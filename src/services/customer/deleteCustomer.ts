14910f016e0af4f324446bc8146762f0db8c0a4f
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-09 21:14:28 -0300

    Added types for DeleteCustomerUseCase

--- src/services/customer/deleteCustomer.ts
+++ src/services/customer/deleteCustomer.ts
@@ -1,3 +1,4 @@
+import { CustomerInterface } from "../../interfaces/entities.interfaces/customer.interface";
 import { CustomerRepositoryInterface } from "../../interfaces/repositories.interfaces/customer.repository.interface";
 
 export class DeleteCustomerUseCase {
@@ -7,7 +8,7 @@ export class DeleteCustomerUseCase {
     this.repository = repository;
   }
 
-  async execute(customerId: string) {
+  async execute(customerId: string): Promise<CustomerInterface> {
     const deletedCustomer = await this.repository.deleteCustomer(customerId);
     if (deletedCustomer === undefined) {
       throw new Error("Customer not found to delete.");
