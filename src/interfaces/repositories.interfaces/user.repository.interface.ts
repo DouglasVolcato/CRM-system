2a5fc941165f78946bd3640216a24c0b54796f87
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-09 21:19:54 -0300

    Added types for UserRepositoryInterface

--- src/interfaces/repositories.interfaces/user.repository.interface.ts
+++ src/interfaces/repositories.interfaces/user.repository.interface.ts
@@ -1,20 +1,17 @@
 import { UserInterface } from "../entities.interfaces/user.interface";
 
 export interface UserRepositoryInterface {
-  createUser(userBody: UserInterface): Promise<unknown> | UserInterface;
+  createUser(userBody: UserInterface): Promise<UserInterface>;
 
-  getAllUsers(): Promise<unknown> | UserInterface[];
+  getAllUsers(): Promise<UserInterface[]>;
 
-  getUserByName(userName: string): Promise<unknown> | UserInterface[];
+  getUserByName(userName: string): Promise<UserInterface[]>;
 
-  getUserById(userId: string): Promise<unknown> | UserInterface;
+  getUserById(userId: string): Promise<UserInterface>;
 
-  getUserByEmail(userEmail: string): Promise<unknown> | UserInterface;
+  getUserByEmail(userEmail: string): Promise<UserInterface>;
 
-  deleteUser(userId: string): Promise<unknown> | UserInterface;
+  deleteUser(userId: string): Promise<UserInterface>;
 
-  updateUser(
-    userId: string,
-    userBody: UserInterface
-  ): Promise<unknown> | UserInterface;
+  updateUser(userId: string, userBody: UserInterface): Promise<UserInterface>;
 }
