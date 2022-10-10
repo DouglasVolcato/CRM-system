effa77f4e4dfffbac6adc3abd50c6cfd41490e52
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-09 21:12:01 -0300

    Added argument types for GetUserByEmailUseCase

--- src/services/user/getUserByEmail.ts
+++ src/services/user/getUserByEmail.ts
@@ -1,3 +1,4 @@
+import { UserInterface } from "../../interfaces/entities.interfaces/user.interface";
 import { UserRepositoryInterface } from "../../interfaces/repositories.interfaces/user.repository.interface";
 
 export class GetUserByEmailUseCase {
@@ -7,7 +8,7 @@ export class GetUserByEmailUseCase {
     this.repository = repository;
   }
 
-  async execute(userEmail: string) {
+  async execute(userEmail: string): Promise<UserInterface> {
     const foundUser = await this.repository.getUserByEmail(userEmail);
     if (foundUser === undefined) {
       throw new Error("User not found.");
