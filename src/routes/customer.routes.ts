9907c4c5ad74f649fc8ddffb20d14afcd040c389
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-09 21:17:04 -0300

    Added types for CustomerRoutes

--- src/routes/customer.routes.ts
+++ src/routes/customer.routes.ts
@@ -20,43 +20,55 @@ export class CustomerRoutes {
     this.res = res;
   }
 
-  async route() {
+  async route(): Promise<void | http.ServerResponse<http.IncomingMessage>> {
     if (
       this.router === "/customers/create-customer" &&
       this.req.method === "POST"
     ) {
-      await authMiddleware(this.req, this.res);
-      this.controller.createCustomerController(this.req, this.res);
+      const middleware = await authMiddleware(this.req, this.res);
+      if (middleware) {
+        this.controller.createCustomerController(this.req, this.res);
+      }
     } else if (
       this.router.includes("/customers/delete-customer/") &&
       this.req.method === "DELETE"
     ) {
-      await authMiddleware(this.req, this.res);
-      this.controller.deleteCustomerController(this.req, this.res);
+      const middleware = await authMiddleware(this.req, this.res);
+      if (middleware) {
+        this.controller.deleteCustomerController(this.req, this.res);
+      }
     } else if (
       this.router.includes("/customers/find-customer-by-id/") &&
       this.req.method === "GET"
     ) {
-      await authMiddleware(this.req, this.res);
-      this.controller.getCustomerByIdController(this.req, this.res);
+      const middleware = await authMiddleware(this.req, this.res);
+      if (middleware) {
+        this.controller.getCustomerByIdController(this.req, this.res);
+      }
     } else if (
       this.router === "/customers/find-all-customers" &&
       this.req.method === "GET"
     ) {
-      await authMiddleware(this.req, this.res);
-      this.controller.getAllCustomersController(this.req, this.res);
+      const middleware = await authMiddleware(this.req, this.res);
+      if (middleware) {
+        this.controller.getAllCustomersController(this.req, this.res);
+      }
     } else if (
       this.router.includes("/customers/find-customers-by-name/") &&
       this.req.method === "GET"
     ) {
-      await authMiddleware(this.req, this.res);
-      this.controller.getCustomerByNameController(this.req, this.res);
+      const middleware = await authMiddleware(this.req, this.res);
+      if (middleware) {
+        this.controller.getCustomerByNameController(this.req, this.res);
+      }
     } else if (
       this.router.includes("/customers/update-customer/") &&
       this.req.method === "PUT"
     ) {
-      await authMiddleware(this.req, this.res);
-      this.controller.updateCustomerController(this.req, this.res);
+      const middleware = await authMiddleware(this.req, this.res);
+      if (middleware) {
+        this.controller.updateCustomerController(this.req, this.res);
+      }
     } else {
       this.res.writeHead(400, { "Content-Type": "application/json" });
       this.res.end(JSON.stringify({ message: "Route not found" }));
