1b790c88f0feb0febe3d3fdb6a861ab61cf05de1
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-09 21:20:23 -0300

    Added types for UserControllerInterface

--- src/interfaces/controllers.interfaces/user.controller.interface.ts
+++ src/interfaces/controllers.interfaces/user.controller.interface.ts
@@ -4,35 +4,35 @@ export interface UserControllerInterface {
   createUserController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): Promise<unknown> | http.IncomingMessage;
+  ): Promise<http.IncomingMessage | http.ServerResponse>;
 
   deleteUserController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): Promise<unknown> | http.ServerResponse;
+  ): Promise<http.IncomingMessage | http.ServerResponse>;
 
   getAllUsersController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): Promise<unknown> | http.ServerResponse;
+  ): Promise<http.IncomingMessage | http.ServerResponse>;
 
   getUserByIdController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): Promise<unknown> | http.ServerResponse;
+  ): Promise<http.IncomingMessage | http.ServerResponse>;
 
   getUserByEmailController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): Promise<unknown> | http.ServerResponse;
+  ): Promise<http.IncomingMessage | http.ServerResponse>;
 
   getUserByNameController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): Promise<unknown> | http.ServerResponse;
+  ): Promise<http.IncomingMessage | http.ServerResponse>;
 
   updateUserController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): Promise<unknown> | http.IncomingMessage;
+  ): Promise<http.IncomingMessage | http.ServerResponse>;
 }
