cf54ccb00dadd94f10da55a9d9e5877c8fd5fec6
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-09 21:20:32 -0300

    Added types for CustomerControllerInterface

--- src/interfaces/controllers.interfaces/customer.controller.interface.ts
+++ src/interfaces/controllers.interfaces/customer.controller.interface.ts
@@ -4,30 +4,30 @@ export interface CustomerControllerInterface {
   createCustomerController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): Promise<unknown> | http.IncomingMessage;
+  ): Promise<http.IncomingMessage | http.ServerResponse>;
 
   deleteCustomerController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): Promise<unknown> | http.ServerResponse;
+  ): Promise<http.IncomingMessage | http.ServerResponse>;
 
   getAllCustomersController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): Promise<unknown> | http.ServerResponse;
+  ): Promise<http.IncomingMessage | http.ServerResponse>;
 
   getCustomerByIdController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): Promise<unknown> | http.ServerResponse;
+  ): Promise<http.IncomingMessage | http.ServerResponse>;
 
   getCustomerByNameController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): Promise<unknown> | http.ServerResponse;
+  ): Promise<http.IncomingMessage | http.ServerResponse>;
 
   updateCustomerController(
     req: http.IncomingMessage,
     res: http.ServerResponse
-  ): Promise<unknown> | http.IncomingMessage;
+  ): Promise<http.IncomingMessage | http.ServerResponse>;
 }
