d6ac210c2f19db1c6ff029389926d2996e8f0289
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-09 21:19:19 -0300

    Added types for CustomerServicesInterface

--- src/interfaces/services.interfaces/customer.services.interface.ts
+++ src/interfaces/services.interfaces/customer.services.interface.ts
@@ -2,31 +2,29 @@ import { CustomerInterface } from "../entities.interfaces/customer.interface";
 
 export interface CustomerServicesInterface {
   createCustomerUseCase: {
-    execute(
-      customerBody: CustomerInterface
-    ): Promise<unknown> | CustomerInterface;
+    execute(customerBody: CustomerInterface): Promise<CustomerInterface>;
   };
 
   deleteCustomerUseCase: {
-    execute(customerId: string): Promise<unknown> | CustomerInterface;
+    execute(customerId: string): Promise<CustomerInterface>;
   };
 
   getAllCustomerUseCase: {
-    execute(): Promise<unknown> | CustomerInterface[];
+    execute(): Promise<CustomerInterface[]>;
   };
 
   getCustomerByIdUseCase: {
-    execute(customerId: string): Promise<unknown> | CustomerInterface;
+    execute(customerId: string): Promise<CustomerInterface>;
   };
 
   getCustomerByNameUseCase: {
-    execute(customerName: string): Promise<unknown> | CustomerInterface[];
+    execute(customerName: string): Promise<CustomerInterface[]>;
   };
 
   updateCustomerUseCase: {
     execute(
       customerId: string,
       customerBody: CustomerInterface
-    ): Promise<unknown> | CustomerInterface;
+    ): Promise<CustomerInterface>;
   };
 }
